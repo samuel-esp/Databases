720ba2683c97658972ec610ca306cb8d2c3affc4
Samuel Esposito <40698384+samuel-esp@users.noreply.github.com>
2019-11-15 21:45:20 +0100

    Update esonero18.sql

--- esonero18.sql
+++ esonero18.sql
@@ -1,3 +1,7 @@
+====================================================================================
+
+/*DATABASE USATO PER LE PROVE SQL*/
+
 create schema if not exists "esonero18";
 set search_path to "esonero18";
 
@@ -37,3 +41,89 @@ insert into bollette values(01, 10, '2019-01-01', 35, '2019-01-02');
 insert into bollette values(02, 11, '2019-01-01', 40, '2019-01-03');
 insert into bollette values(03, 10, '2019-05-01', 35, '2019-01-07');
 insert into bollette values(04, 10, '2019-06-01', 37, '2019-01-09');
+insert into bollette values(05, 10, '2019-06-01', 37, null);
+
+=========================================================================
+ESERCIZIO 1 -- CARDINALITA
+
+/*DOMANDA 1 SQL*/
+
+SELECT *
+FROM utenze JOIN persone ON titolare=CF
+WHERE importo > 100
+
+MIN SIMBOLI: 0 
+MIN VALORE: 0
+MAX SIMBOLI: B 
+MAX VALORE: 100000
+
+(Join tra key e reference seguito da una selezione)
+
+
+/*DOMANDA 2 SQL*/
+
+SELECT *
+FROM bollette JOIN utenze ON utenza=codice
+JOIN persone ON titolare=CF
+WHERE importo > 100
+
+MIN SIMBOLI: 0 
+MIN VALORE: 0
+MAX SIMBOLI: B 
+MAX VALORE: 100000 
+
+(Join con key e reference seguito da una selezione)
+
+/*DOMANDA 3 SQL*/
+
+SELECT codice, indirizzo, SUM(importo) AS totale
+FROM utenze JOIN bollette ON codice = utenza
+GROUP BY codice, indirizzo
+
+MIN SIMBOLI: 1 
+MIN VALORE: 1
+MAX SIMBOLI: U
+MAX VALORE: 20000 
+
+(Raggruppamento per key di R2 dopo aver eseguito un Join con key e reference)
+
+==========================================================================
+
+ESERCIZIO 3 CARDINALITA'
+
+/*DOMANDA 1*/
+
+CHECK ( ( ( Netto = StipLordo - Trattenute) AND (OK = ’true’))
+OR ((Netto <> StipLordo - Trattenute) AND (OK = ’false’ ) ) )
+
+Questa e' una condizione di "se e solo" se di conseguenza le tabelle che la soddisfano saranno:
+
+A: NO
+B: SI 
+C: NO
+D: NO
+
+
+/*DOMANDA 2*/
+
+CHECK ( ( NOT (OK = ’true’) ) OR ( Netto = Lordo - Tasse ) )
+
+Questa condizione dice che se OK vale true allora il netto deve per forza valere quella differenza, non specifica cosa accade per valori diversi da false; le tabelle che la soddisfano saranno:
+
+A: NO
+B: SI 
+C: SI
+D: SI
+
+/*DOMANDA 3*/
+
+CHECK ( NOT( Netto = Lordo - Tasse ) ) OR ( ( (OK = ’true’) )
+
+Questa condizione dice che se netto e' eguale a quella differenza OK deve essere per forza valere true, non specifica cosa succede altrimenti; le tabelle che la soddisfano saranno:
+
+A: SI
+B: SI 
+C: NO
+D: NO
+
+
